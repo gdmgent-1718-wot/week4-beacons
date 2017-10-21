624225ef52bb266596464863c4ad9eee8021b2f0
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-10-21 14:35:11 +0200

    Added seeding function

--- Backend/README.md
+++ Backend/README.md
@@ -1,5 +1,4 @@
-## Server 
-
+## Server  
 #### Software 
 Voor de server gebruiken we Nodejs, Express en MongoDB. 
 Express is een framework waarmee je webapplicaties kan maken bovenop nodejs. Het vergemakkelijkt het     opzet process om een server te doen draaien. 
@@ -7,8 +6,8 @@ Express is een framework waarmee je webapplicaties kan maken bovenop nodejs. Het
 Mongodb is een database die data opslaat als flexibele json documenten. Dit wil zeggen dat het een zeer
 flexiebel systeem is. 
 
-**Automatisatie** 
 
+#### Automatisatie
 Als je niet steeds na elke aanpassing je server wil herstarten dan kan je best gebruik maken van nodemon
 ```
 npm install nodemon --save-dev
@@ -16,11 +15,11 @@ npm install nodemon --save-dev
 Vervolgens voeg onderstaand stukje code toe aan de package.json. Dit gaat het commando om de server te starten vereenvoudigen. 
 ```json
 {
-  // ...
   "scripts": {
     "dev": "nodemon server.js"
   }
-  // ...
 }
 ```
 Start de server met ```npm run dev```
+
+#### database 
--- Backend/server.js
+++ Backend/server.js
@@ -2,14 +2,17 @@ const express = require('express'); // import express
 const app = express(); 
 const MongoClient = require('mongodb').MongoClient // import mongodb
 const PubNub = require('pubnub'); // import mongodb
-var db; 
+const pubnub = new PubNub({
+    publishKey : 'pub-c-e8c3fb0e-1f41-4b92-a3e5-30cb96805ed7',
+    subscribeKey : 'sub-c-e9f3f922-b32d-11e7-a852-92b7c98bd364'
+});
+var db; var db_name = 'promotions';
 path = __dirname + '/views/';
-var pubnubmessage = {'message': 'My message'};
-
 // Setup the database
-MongoClient.connect('mongodb://localhost:27017/beackon', (err, database) => {
+MongoClient.connect('mongodb://localhost:27017/' + db_name, (err, database) => {
     if (err) return console.log(err);
     db = database;
+    console.log(db['s']['databaseName']);
     // Create a server where browsers can connect to. 
     app.listen(3000, function(){ 
         console.log('listening on localhost:3000');
@@ -18,51 +21,61 @@ MongoClient.connect('mongodb://localhost:27017/beackon', (err, database) => {
 
 // Get request
 app.get('/', (req, res) => {
-    db.collection('beaconworld').find().toArray(function(err, results) {
+    var collections = ['shoes', 'toys', 'cosmetics', 'undies'];
+    db.collection('shoes').find().toArray(function(err, results) {
         console.log(results);
         res.sendFile(path + 'index.html'); // Show the index.html page listed in the views folder. 
     });
    
 })
-app.get('/data', (req, res) => {
-    publish();
+app.get('/:channel', (req, res) => {
+    publish(req.params.channel);
 });
 
-function publish (){
-    var message;
-    pubnub = new PubNub({
-        publishKey : 'pub-c-e8c3fb0e-1f41-4b92-a3e5-30cb96805ed7',
-        subscribeKey : 'sub-c-e9f3f922-b32d-11e7-a852-92b7c98bd364'
-    })
-       
-    function publishSampleMessage() {
-        var publishConfig = {
-            channel : "beaconworld",
-            message : "My winning power"
-        }
-        pubnub.publish(publishConfig, function(status, response) {
-            console.log(status, response);
-        })
+app.get('/populate_the_database', (req, res) => {
+    var collections = ['shoes', 'toys', 'cosmetics', 'undies'];
+    var promotions = {'nike': "-30%"};
+    collections.forEach(function(item) {
+        db.collection(item).save(promotions, (err, result)=> {
+            if(err) return console.log(err);
+      
+            console.log('Saved '+ item + ' to database');
+        });
+    }, this);
+});
+
+function publish(chl){
+    console.log("publish");
+
+    var publishConfig = {
+        channel : chl,
+        message : "My winning power"
     }
-       
     pubnub.addListener({
         status: function(statusEvent) {
             if (statusEvent.category === "PNConnectedCategory") {
-                publishSampleMessage();
+                console.log("status");
+                pubnub.publish(publishConfig, function(status, response) {
+                    console.log(status, response);
+                })
             }
         },
         message: function(message) {
-            db.collection(message['channel']).save(message, (err, result) => {
-                if (err) return console.log(err);
-                console.log(message);
-                console.log('Saved to database');
-            })
+            db.collection(chl).find().toArray(function(err, results) {
+                console.log(results);
+            });
         },
         presence: function(presenceEvent) {
+            console.log("PRESENCE", presenceEvent);
         }
-    })      
+    })
+    
+    subscibe(chl);
+}
+
+function subscibe(chl){
     console.log("Subscribing...");
     pubnub.subscribe({
-        channels: ['beaconworld'] 
+        channels: [chl] 
     });
 }
