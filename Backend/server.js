e07a413c081c132a154d1f83861ac2c363adf97f
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-10-21 13:32:04 +0200

    Added pubnub to the server. Updated readme.

--- Backend/server.js
+++ Backend/server.js
@@ -1,19 +1,13 @@
 const express = require('express'); // import express
 const app = express(); 
 const MongoClient = require('mongodb').MongoClient // import mongodb
+const PubNub = require('pubnub'); // import mongodb
 var db; 
 path = __dirname + '/views/';
+var pubnubmessage = {'message': 'My message'};
 
-
-// Get request
-app.get('/', (req, res) => {
-    res.sendFile(path + 'index.html'); // Show the index.html page listed in the views folder. 
-})
-app.post('/data', (req, res) => {
-    // Establish pubnub connection. 
-});
 // Setup the database
-MongoClient.connect('myDatabase', (err, database) => {
+MongoClient.connect('mongodb://localhost:27017/beackon', (err, database) => {
     if (err) return console.log(err);
     db = database;
     // Create a server where browsers can connect to. 
@@ -22,3 +16,53 @@ MongoClient.connect('myDatabase', (err, database) => {
     });
 })
 
+// Get request
+app.get('/', (req, res) => {
+    db.collection('beaconworld').find().toArray(function(err, results) {
+        console.log(results);
+        res.sendFile(path + 'index.html'); // Show the index.html page listed in the views folder. 
+    });
+   
+})
+app.get('/data', (req, res) => {
+    publish();
+});
+
+function publish (){
+    var message;
+    pubnub = new PubNub({
+        publishKey : 'pub-c-e8c3fb0e-1f41-4b92-a3e5-30cb96805ed7',
+        subscribeKey : 'sub-c-e9f3f922-b32d-11e7-a852-92b7c98bd364'
+    })
+       
+    function publishSampleMessage() {
+        var publishConfig = {
+            channel : "beaconworld",
+            message : "My winning power"
+        }
+        pubnub.publish(publishConfig, function(status, response) {
+            console.log(status, response);
+        })
+    }
+       
+    pubnub.addListener({
+        status: function(statusEvent) {
+            if (statusEvent.category === "PNConnectedCategory") {
+                publishSampleMessage();
+            }
+        },
+        message: function(message) {
+            db.collection(message['channel']).save(message, (err, result) => {
+                if (err) return console.log(err);
+                console.log(message);
+                console.log('Saved to database');
+            })
+        },
+        presence: function(presenceEvent) {
+        }
+    })      
+    console.log("Subscribing...");
+    pubnub.subscribe({
+        channels: ['beaconworld'] 
+    });
+}
