7fd48d828edeb19a5528afc216b4edf4164ebc1c
Lotte Verwerft <lotte.verwerft@student.arteveldehs.be>
2017-10-25 11:55:39 +0200

    backend ui

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,9 @@ const pubnub = new PubNub({
     subscribeKey : 'sub-c-e9f3f922-b32d-11e7-a852-92b7c98bd364'
 });
 var db; var db_name = 'promotions';
+var promo = []; 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'pug');
 
 // Setup the database
 MongoClient.connect('mongodb://localhost:27017/' + db_name, (err, database) => {
@@ -15,21 +18,31 @@ MongoClient.connect('mongodb://localhost:27017/' + db_name, (err, database) => {
     console.log(db['s']['databaseName']);
     // Create a server where browsers can connect to. 
     app.listen(3000, function(){ 
-        console.log('listening on localhost:3000');
+        console.log('listening on http://localhost:3000/');
     });
 })
 
+app.get('/', (req, res) => {
+    res.render('index', { title: 'Hey', message: 'Hello there!' });
+ });
+ 
 app.get('/promotions/:channel', (req, res) => {
     var channel = req.params.channel;
-    var message = [];
+    var message = [];  
     db.collection(channel).find({}).toArray((err, result) => {
         if (err) console.log(err);
-        console.log(result); 
         message = result;
         publish(channel, message);
-    });
+        res.render("product", { title: 'promo', message: promo, channel: channel });
+    }); 
 });
-
+app.get('/toy', (req, res) => {
+    db.collection("toys").find({}).toArray(function(err, result) {
+      if (err) console.log(err);
+      console.log(result);
+      db.close();
+    });
+})
 app.get('/populate_the_database', (req, res) => {
     /**
      * Promotion structure:
@@ -43,14 +56,14 @@ app.get('/populate_the_database', (req, res) => {
      */
     var shoes = [{
             "brand": "Nike",
-            "precentage": "10",
-            "product": "Sneakers revolution 3", 
-            "ends": "20.11.2017"
-        }, {
-            "brand": "Mustang",
-            "precentage": "30",
-            "product": "Cotton sneakers", 
+            "precentage": "15",
+            "product": "Sneakers revolution 2", 
             "ends": "10.11.2017"
+        }, {
+            "brand": "Hush puppies",
+            "precentage": "30",
+            "product": "sneakers", 
+            "ends": "10.12.2017"
         }, 
     ];
     shoes.forEach(function(shoe) {
@@ -60,17 +73,35 @@ app.get('/populate_the_database', (req, res) => {
          });
     }, this);
 
+    var toys = [{
+        "brand": "Jupiler League",
+        "precentage": "40",
+        "product": "ball", 
+        "ends": "20.01.2018"
+    }, {
+        "brand": "Monk",
+        "precentage": "30",
+        "product": "doll", 
+        "ends": "1.11.2017"
+    }];
+    toys.forEach(function(toy) {
+        db.collection('toys').save(toy, (err, result)=> {
+            if(err) return console.log(err);
+            console.log('Saved toys to database');
+        });
+    }, this);
+
 });
 
 app.get('/delete/:collection', (req, res) => {
     var collection = req.params.collection;
     db.collection(collection).drop(function(err, delOK) {
-        if (err) throw err;
+        if (err) console.loge(err);
         if (delOK) console.log("Deleted: " + collection);
       });
-})
+});
 function publish(chl, promotions){
-
+    
     var publishConfig = {
         channel : chl,
         message : promotions // ALL FOUND PROMOTIONS FOR ONE CHANNEL
@@ -78,18 +109,15 @@ function publish(chl, promotions){
     pubnub.addListener({
         status: function(statusEvent) {
             if (statusEvent.category === "PNConnectedCategory") {
-                console.log("status");
                 pubnub.publish(publishConfig, function(status, response) {
                     console.log(status, response);
                 })
             }
         },
-        // RETURN THE RESÙTLS TO THE CHANNEL AKA SEND TO IONIC APP
+        // RETURN THE RESÙTLS TO THE CHANNEL 
         message: function(message) {
-            console.log("MESSAGE", message["message"]);
-        },
-        presence: function(presenceEvent) {
-            console.log("PRESENCE", presenceEvent);
+            console.log(message['message']);
+            promo = message['message'];
         }
     })
     subscibe(chl);
